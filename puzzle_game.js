0ce1ed2ec2783ffd59fdca8ec9233af8f98c59c0
Alexandra <skripka23@mail.ru>
2019-09-17 16:48:40 +0400

    15 puzzle game is okay by clicks

--- puzzle_game.js
+++ puzzle_game.js
@@ -1,31 +1,16 @@
-// реализует игру
-function game() {
-	// игровое поле
-	var gameField = [];
-	for (var i = 0; i < 4; i++){
-		gameField[i] = [];
-		for (var j = 0; j < 4; j++){
-			if (i + j != 6){
-				gameField[i][j] = i * 4 + j + 1;
-			} else {
-				gameField[i][j] = "";
-			}
-		}
-	} 
+// игровое поле
+var gameField = [
+	 [1,2,3,4],
+	 [5,6,7,8],
+	 [9,10,11,12],
+	 [13,14,15,0]
+	 ];
 
-	gameField = shuffle(350, gameField);
-	createField(gameField);
-		
-}
-
-function getNullCell(gameField){
-	for (var i = 0; i < 4; i++) {
-		for (var j = 0; j < 4; j++) {
-			if (gameField[i][j] == "")
-				return {'x': i, 'y': j}; 
-		}
-	}
-}
+var field;
+let nullCell = { 
+  x: 3,  
+  y: 3       
+};
 
 // обменивает местами ячейки
 function swapCells(gameField, x1, y1, x2, y2){
@@ -35,16 +20,9 @@ function swapCells(gameField, x1, y1, x2, y2){
 }
 
 // перемешивает ячейки пятнашек
-function shuffle(count, gameField){
-	var nullCell = getNullCell(gameField);
-	for (var i = 0; i < count; i++) {
-		switch(Math.rount(3 * Math.random())) {
-		/* 0 соответсвует верхней соседней костяшке, 1 - правой  и т.д.
-		 * обратим внимание что обмен местами, например,
-		 * с верхней костяшкой возможен, если "пустое место"
-		 * не находится у верхней границы игрового поля. Аналогично и для
-		 * других соседних костяшек. При обмене изменяем переменные ei и ej.
-		 */
+function shuffle(count) {
+	for (i = 0; i < count; i++) {
+		switch(Math.round(3 * Math.random())) {
 		 case 0: 
 			if (nullCell.x != 0) {
 				swapCells(gameField, nullCell.x, nullCell.y, --nullCell.x, nullCell.y);
@@ -68,17 +46,18 @@ function shuffle(count, gameField){
 		 
 		}
 	}
-	return gameField;
 }
 
 //создаем таблицу в которой рисуем ячейки
-function createField(gameField){
+function createField(){
 	// создаем таблицу
 	var table = document.createElement("table");
-	for(var i = 0; i < 4; i++){
-	// добавляем в нее строки
-	var row = document.createElement("tr");
-		for(var j = 0; j < 4; j++) {
+	var tbody = document.createElement("tbody");
+	table.appendChild(tbody);
+	for(i = 0; i < 4; i++) {
+		// добавляем в нее строки
+		var row = document.createElement("tr");
+		for(j = 0; j < 4; j++) {
 			// создаем ячейки для пятнашек
 			var cell = document.createElement("td");
 			cell.id = i + " " + j;
@@ -89,7 +68,7 @@ function createField(gameField){
 			//добавляем ячейку к игровому полю
 			row.appendChild(cell);
 			}
-		table.appendChild(row);		
+		tbody.appendChild(row);		
 	}
 	// проверка на случай, если игрок начнет игру сначала
 	if(field.childNodes.length == 1) {
@@ -98,4 +77,46 @@ function createField(gameField){
 	}
 	// записываем таблицу в div		
 	field.appendChild(table);
+}
+
+window.onload = function() {
+	field = document.getElementById("field");
+	game();				
+	document.getElementById("play").onclick = game;
+}
+
+function cellClick(event){	
+	var event = event || window.event;
+	cell = event.srcElement || event.target;
+	// номер строки
+	x = cell.id.charAt(0);
+	// номер столбца
+	y = cell.id.charAt(2);
+	console.log(x, " ", y);
+	// меняем местами пустую ячейку с выбранной,
+	// если расстояние между ними равно 1
+	// и они в одном столбца или строке
+	if((x == nullCell.x && Math.abs(y - nullCell.y) == 1) || (y == nullCell.y && Math.abs(x - nullCell.x) == 1)){
+		document.getElementById(nullCell.x + " " + nullCell.y).innerHTML = cell.innerHTML;
+		cell.innerHTML = "";
+		nullCell.x = x;
+		nullCell.y = y;
+		var q = true;
+		//Проверяем не в выигрышной ли комбинации находятся ячейки.
+		for(i = 0; i < 4; i++) {
+			for(j = 0; j < 4; j++) {
+				if (i + j != 6 && document.getElementById(i + " " + j).innerHTML != i * 4 + j + 1){
+					q = false;
+					break;
+				}
+			}
+		}
+		if (q)
+			alert("YOU WIN!");
+	}
+}
+	
+function game() {
+	shuffle(350);
+	createField();
 }
